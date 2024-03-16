60ab340ef605d74b7f555f1d8444c4288b2c5fa9
SomaTakata <somatakata@gmail.com>
2024-03-16 15:56:13 +0900

    ヒーローセクション完成

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
+import HeroSection from "./_components/HeroSection";
 import Navbar from "./_components/Navbar";
+import Skills from "./_components/Skills";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-black">
-      <div className="max-w-7xl mx-auto">
-        <Navbar />
+    <div className="min-h-[400vh] bg-black over-hidden">
+      <div className=" dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]">
+        <div className="max-w-7xl mx-auto p-5 ">
+          <Navbar />
+          <HeroSection />
+        </div>
+        <div className="h-10 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full"></div>
+      </div>
+      <div className="max-w-7xl mx-auto p-5 mt-20">
+        <Skills />
       </div>
     </div>
   );
