c96a46bcdce996abfc7137816caf8128dc9c6ce8
SomaTakata <somatakata@gmail.com>
2024-03-29 01:24:15 +0900

    svg animation

--- app/(home)/projects/page.tsx
+++ app/(home)/projects/page.tsx
@@ -16,6 +16,19 @@ const gridSquareVariants = {
   show: { opacity: 1 },
 };
 
+const svgIconVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(252, 211,77,0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(252, 211,77,1)",
+  },
+};
+
 export default function page() {
   const { scrollYProgress: completionProgress } = useScroll();
   return (
@@ -120,9 +133,9 @@ export default function page() {
           className="bg-gray-200/30 backdrop-blur-lg
     border border-gray-200/30 shadow-lg aspect-square rounded-lg justify-center flex items-center gap-10"
         >
-          <motion.div className="w-1/2 aspect-square bg-orange-500 rounded-xl">
+          <motion.div className="w-1/2 aspect-square bg-gray-800 rounded-xl">
             <motion.div
-              className="w-full bg-gray-800 rounded-xl h-full origin-bottom"
+              className="w-full bg-orange-500 rounded-xl h-full origin-bottom"
               style={{ scaleY: completionProgress }}
             ></motion.div>
           </motion.div>
@@ -131,7 +144,39 @@ export default function page() {
           variants={gridSquareVariants}
           className="bg-gray-200/30 backdrop-blur-lg
     border border-gray-200/30 shadow-lg aspect-square rounded-lg justify-center flex items-center gap-10"
-        ></motion.div>
+        >
+          <motion.svg
+            xmlns="https://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-1/2 stroke-amber-500 stroke-[0.5]
+            "
+          >
+            <motion.path
+              d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+              variants={svgIconVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{
+                default: {
+                  duration: 2,
+                  ease: "easeInOut",
+                  delay: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+                fill: {
+                  duration: 2,
+                  ease: "easeIn",
+                  delay: 2,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 1,
+                },
+              }}
+            />
+          </motion.svg>
+        </motion.div>
       </motion.section>
     </motion.div>
   );
