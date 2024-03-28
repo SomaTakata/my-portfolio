17a0fb69e7a572f7e6ae84cfbd9eb80cf3a9f3b2
SomaTakata <somatakata@gmail.com>
2024-03-29 01:03:14 +0900

    Drag

--- app/(home)/projects/page.tsx
+++ app/(home)/projects/page.tsx
@@ -101,7 +101,19 @@ export default function page() {
           variants={gridSquareVariants}
           className="bg-gray-200/30 backdrop-blur-lg
     border border-gray-200/30 shadow-lg aspect-square rounded-lg justify-center flex items-center gap-10"
-        ></motion.div>
+        >
+          <motion.div
+            className="w-1/3 h-1/3 bg-orange-500 rounded-3xl cursor-grab "
+            drag
+            dragConstraints={{
+              top: -125,
+              right: 125,
+              bottom: 125,
+              left: -125,
+            }}
+            dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+          ></motion.div>
+        </motion.div>
         <motion.div
           variants={gridSquareVariants}
           className="bg-gray-200/30 backdrop-blur-lg
